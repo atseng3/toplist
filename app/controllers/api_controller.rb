e6b77c1d412275b42e20cf3242c97122779b27e3
Albert Tseng <albert@chegg.com>
2015-10-28 15:59:32 -0700

    update UI when people click approve

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,12 +1,12 @@
 class ApiController < ApplicationController
 	def approve
 		# request to this action will attempt to like a post for the current user
-		respond_to do |format|
-      # format.html { redirect_to posts_url }
-      format.json { render json: approve_params['post_id'] }
-    end
 		@post = Post.find(approve_params['post_id'])
 		@post.liked_by current_user
+
+		respond_to do |format|
+      format.json { render json: @post.votes_for.count }
+    end
 	end
 
 	private
